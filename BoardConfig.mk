9587088d1778cf31569db14c86e5f4f8034f4c26
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-07-28 17:24:02 +0000

    moonstone: vintf: Decommonize device specific changes
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,9 +13,12 @@ TARGET_SCREEN_DENSITY := 440
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_moonstone
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_moonstone
 
-# ODM Manifest
+# Vintf Manifest
 ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/configs/vintf/manifest_nfc.xml
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/xiaomi_framework_compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/xiaomi_manifest.xml
 
 # OTA Assert
 TARGET_OTA_ASSERT_DEVICE := moonstone,sunstone
